77b8aaa27c5521fea99307e33c0e41fe645b75bd
SanjulaD <desanjula99@gmail.com>
2020-11-02 11:25:08 +0530

    initial commit

---
+++ src/products/management/Product.java
@@ -0,0 +1,43 @@
+package products.management;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private float price;
+    private String addDate;
+    private byte[] image;
+
+    public Product(int pid, String pname, float pPrice, String pAddDate, byte[] pImg) {
+        this.id = pid;
+        this.name = pname;
+        this.price = pPrice;
+        this.addDate = pAddDate;
+        this.image = pImg;
+    }
+    
+    public int getId()
+    {
+        return  id;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public float getPrice()
+    {
+        return price;
+    }
+    
+    public String getAddDate()
+    {
+        return addDate;
+    }
+    
+    public byte[] getImage()
+    {
+        return image; 
+    }
+}
